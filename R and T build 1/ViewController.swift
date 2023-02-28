d3715377aef8332f76f7982132f127467cf3e6d5
Kazimir Makaranka <m2014ky@gmail.com>
2023-02-28 10:55:43 +0300

    Initial Commit

---
+++ R and T build 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  R and T build 1
+//
+//  Created by Kazimir on 28.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
